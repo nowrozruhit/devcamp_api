c2ff069ad7c0f25efbde4e0da2647f4cf3ad3bf4
Nowroz_Ruhit <nowrozjunaedrahman.com>
2021-04-07 01:39:18 +0600

    created get and post route

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ dotenv.config({ path:'./config/config.env' });
 
 const app = express();
 
+app.get('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({success:true, msg:"Show all bootcamps"});
+});
+
+app.post('/api/v1/bootcamps', (req, res) => {
+    res.status(200).json({ success:true, msg:"Create new bootcamp" });
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`));
