8c8c266a6712197fcd835f2415e22a25440b80f1
Nowroz_Ruhit <nowrozjunaedrahman.com>
2021-04-09 17:50:37 +0600

    done till Bootcamp model

--- server.js
+++ server.js
@@ -1,19 +1,35 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
 
 // Load env vars
-dotenv.config({ path:'./config/config.env' });
+dotenv.config({path: './config/config.env'});
+
+// Connect to database
+connectDB();
+
+// load routes
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
-app.get('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json({success:true, msg:"Show all bootcamps"});
-});
+// run dev logging middleware
+if(process.env.NODE_ENV === 'Developement'){
+    app.use(morgan('dev'));
+}
 
-app.post('/api/v1/bootcamps', (req, res) => {
-    res.status(200).json({ success:true, msg:"Create new bootcamp" });
-});
+// mount the routes
+app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`));
+const server = app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${process.env.PORT}`.yellow.bold))
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // close server and exit process
+    server.close(() => process.exit(1));
+});
