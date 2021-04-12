89d275199d6e00b447e3e0f28e26a7583a28cbac
Nowroz_Ruhit <nowrozjunaedrahman.com>
2021-04-13 00:45:35 +0600

    delete added

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -6,7 +6,7 @@ const Bootcamp = require('../Models/Bootcamp');
 exports.getBootcamps = async (req, res, next) => {
     try {
         const bootcamps = await Bootcamp.find();
-        res.status(200).json({success: true, data: bootcamps});
+        res.status(200).json({success: true, count:bootcamps.length, data: bootcamps});
     } catch (err) {
         res.status(400).json({success: false, msg: err.message});
     }
@@ -16,9 +16,9 @@ exports.getBootcamps = async (req, res, next) => {
 // @route GET /api/v1/bootcamps/:id
 // @access Public
 exports.getBootcamp = async (req, res, next) => {
-    try{
+    try {
         const bootcamp = await Bootcamp.findById(req.params.id);
-        if(!bootcamp){
+        if (!bootcamp) {
             return res.status(400).json({success: false});
         }
         res.status(200).json({success: true, data: bootcamp});
@@ -49,18 +49,26 @@ exports.updateBootcamp = async (req, res, next) => {
             new: true,
             runValidators: true,
         });
-        if(!bootcamp) {
+        if (!bootcamp) {
             return res.status(400).json({success: false});
         }
         res.status(200).json({success: true, data: bootcamp});
     } catch (err) {
-            res.status(400).json({success: false, msg: err.message});
+        res.status(400).json({success: false, msg: err.message});
     }
 }
 
 // @desc Delete bootcamp
 // @route DELETE /api/v1/bootcamps/:id
 // @access Private
-exports.deleteBootcamp = (req, res, next) => {
-    res.status(200).json({success: true, msg: `Delete bootcamp ${req.params.id}`});
+exports.deleteBootcamp = async (req, res, next) => {
+    try {
+        const bootcamp = await Bootcamp.findByIdAndDelete(req.params.id);
+        if (!bootcamp) {
+            return res.status(400).json({success: false});
+        }
+        res.status(200).json({success: true, msg: 'Bootcamp deleted successfully'});
+    } catch (err) {
+        res.status(400).json({success: false, msg: err.message});
+    }
 }
